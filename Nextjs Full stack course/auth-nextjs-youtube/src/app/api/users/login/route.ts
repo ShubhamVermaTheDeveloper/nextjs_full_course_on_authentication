ef4e2b628978453abe450711b426c8ff5ec32c0e
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-02-28 15:06:31 +0530

    get user details

--- Nextjs Full stack course/auth-nextjs-youtube/src/app/api/users/login/route.ts
+++ Nextjs Full stack course/auth-nextjs-youtube/src/app/api/users/login/route.ts
@@ -1,55 +1,57 @@
-"use server"
-import {connect} from  '@/dbConfig/dbConfig';
+"use server";
+import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-import conf from '@/conf';
-
+import conf from "@/conf";
 
 connect();
 
 export async function POST(request: NextResponse) {
   try {
     const reqBody = await request.json();
-    const {email, password} = reqBody;
+    const { email, password } = reqBody;
     console.log(reqBody);
-    
+
     //check if user exists
-    const user = await User.findOne({email})
-    if(!user) {
-      return NextResponse.json({error: "User does not exist"}, {status: 400})
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
     }
 
     console.log("user exists");
-    
+
     // check if password is correct
     const validPassword = await bcryptjs.compare(password, user.password);
     console.log(validPassword);
-    if(!validPassword) {
-      return NextResponse.json({error: "Invalid password"}, {status: 400})
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
     }
     // create taken data
     const tokenData = {
       id: user._id,
       username: user.username,
-      email: user.email
-    }
+      email: user.email,
+    };
     console.log(tokenData);
-    
+
     console.log(process.env.JWT_SECRET_KEY);
     // create token
-    const token = jwt.sign(tokenData, conf.jwtsecretkey!, {expiresIn: "1d"})
+    const token = jwt.sign(tokenData, conf.jwtsecretkey!, { expiresIn: "1d" });
     console.log(token);
     const response = NextResponse.json({
       message: "Login successful",
-      success: true
-    })
-    
+      success: true,
+    });
+
     console.log(response);
-    response.cookies.set("token", token, { httpOnly: true, })
+    response.cookies.set("token", token, { httpOnly: true });
     return response;
   } catch (error: any) {
-    return NextResponse.json({error: error.message}, {status: 500})
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
